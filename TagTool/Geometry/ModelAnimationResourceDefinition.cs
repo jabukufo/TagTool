6d021699d6072f7b8634778ecfb29ff56540bd75
dany5639 <dany_woozie@hotmail.com>
2017-01-16 14:09:02 +0100

    Update ModelAnimationResourceDefinition.cs

--- TagTool/Geometry/ModelAnimationResourceDefinition.cs
+++ TagTool/Geometry/ModelAnimationResourceDefinition.cs
@@ -19,10 +19,10 @@ namespace TagTool.Geometry
             public short FrameCount;
             public sbyte NodeCount;
             public GroupMemberMovementDataType MovementDataType;
-            public PrimaryNodeFlags StaticNodesPrimary;
-            public SecondaryNodeFlags StaticNodesSecondary;
-            public PrimaryNodeFlags AnimatedNodesPrimary;
-            public SecondaryNodeFlags AnimatedNodesSecondary;
+            public GroupMemberFrameEventType FrameEvents;
+            public uint FramesBlockOffset;
+            public int unknown3;
+            public uint NodesFlagsOffset;
             public ResourceDataReference AnimationData;
 
             public enum GroupMemberMovementDataType : sbyte
@@ -35,81 +35,40 @@ namespace TagTool.Geometry
                 x_y_z_absolute,
                 Auto
             }
-
-            [Flags]
-            public enum PrimaryNodeFlags : int
+            public enum GroupMemberFrameEventType : int
             {
-                None = 0,
-                Node0 = 1 << 0,
-                Node1 = 1 << 1,
-                Node2 = 1 << 2,
-                Node3 = 1 << 3,
-                Node4 = 1 << 4,
-                Node5 = 1 << 5,
-                Node6 = 1 << 6,
-                Node7 = 1 << 7,
-                Node8 = 1 << 8,
-                Node9 = 1 << 9,
-                Node10 = 1 << 10,
-                Node11 = 1 << 11,
-                Node12 = 1 << 12,
-                Node13 = 1 << 13,
-                Node14 = 1 << 14,
-                Node15 = 1 << 15,
-                Node16 = 1 << 16,
-                Node17 = 1 << 17,
-                Node18 = 1 << 18,
-                Node19 = 1 << 19,
-                Node20 = 1 << 20,
-                Node21 = 1 << 21,
-                Node22 = 1 << 22,
-                Node23 = 1 << 23,
-                Node24 = 1 << 24,
-                Node25 = 1 << 25,
-                Node26 = 1 << 26,
-                Node27 = 1 << 27,
-                Node28 = 1 << 28,
-                Node29 = 1 << 29,
-                Node30 = 1 << 30,
-                Node31 = 1 << 31
-            }
-
-            [Flags]
-            public enum SecondaryNodeFlags : int
-            {
-                None = 0,
-                Node32 = 1 << 32,
-                Node33 = 1 << 33,
-                Node34 = 1 << 34,
-                Node35 = 1 << 35,
-                Node36 = 1 << 36,
-                Node37 = 1 << 37,
-                Node38 = 1 << 38,
-                Node39 = 1 << 39,
-                Node40 = 1 << 40,
-                Node41 = 1 << 41,
-                Node42 = 1 << 42,
-                Node43 = 1 << 43,
-                Node44 = 1 << 44,
-                Node45 = 1 << 45,
-                Node46 = 1 << 46,
-                Node47 = 1 << 47,
-                Node48 = 1 << 48,
-                Node49 = 1 << 49,
-                Node50 = 1 << 50,
-                Node51 = 1 << 51,
-                Node52 = 1 << 52,
-                Node53 = 1 << 53,
-                Node54 = 1 << 54,
-                Node55 = 1 << 55,
-                Node56 = 1 << 56,
-                Node57 = 1 << 57,
-                Node58 = 1 << 58,
-                Node59 = 1 << 59,
-                Node60 = 1 << 60,
-                Node61 = 1 << 61,
-                Node62 = 1 << 62,
-                Node63 = 1 << 63
+                none = 0,
+                primary_keyframe = 1 << 0,
+                secondary_keyframe = 1 << 1,
+                tertiary_keyframe = 1 << 2,
+                left_foot = 1 << 3,
+                right_foot = 1 << 4,
+                allow_interruption = 1 << 5,
+                do_not_allow_interruption = 1 << 6,
+                both_feet_shuffle = 1 << 7,
+                body_impact = 1 << 8,
+                left_foot_lock = 1 << 9,
+                left_foot_unlock = 1 << 10,
+                right_foot_lock = 1 << 11,
+                right_foot_unlock = 1 << 12,
+                blend_range_marker = 1 << 13,
+                stride_expansion = 1 << 14,
+                stride_contraction = 1 << 15,
+                ragdoll_keyframe = 1 << 16,
+                drop_weapon_keyframe = 1 << 17,
+                match_a = 1 << 18,
+                match_b = 1 << 19,
+                match_c = 1 << 20,
+                match_d = 1 << 21,
+                jetpack_closed = 1 << 22,
+                jetpack_open = 1 << 23,
+                sound_event = 1 << 24,
+                effect_event = 1 << 25,
+                left_hand = 1 << 26,
+                right_hand = 1 << 27,
+                start_bamf = 1 << 28,
+                end_bamf = 1 << 29,
+                hide = 1 << 30
             }
         }
     }
