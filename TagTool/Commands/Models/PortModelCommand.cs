34ab4425a078af5e25e971599c9db28dca7ab6cf
Camden Smallwood <camden.smallwood@gmail.com>
2017-01-29 02:52:07 -0500

    Initial 'PortModel' command implementation

--- TagTool/Commands/Models/PortModelCommand.cs
+++ TagTool/Commands/Models/PortModelCommand.cs
@@ -1,6 +1,14 @@
-﻿using System;
+﻿using Adjutant.Library.Definitions;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using TagTool.Cache;
+using TagTool.Commands.Tags;
+using TagTool.Common;
+using TagTool.Geometry;
+using TagTool.Serialization;
 using TagTool.TagDefinitions;
 
 namespace TagTool.Commands.Models
@@ -17,7 +25,7 @@ namespace TagTool.Commands.Models
                   "PortModel",
                   "Ports a model from H1PC/H1CE/H2X/H3B/H3/ODST cache files.",
                   
-                  "PortModel <Gen3 Map File> <Gen3 Tag>",
+                  "PortModel <Map File> <Tag Path>",
 
                   "Ports a model from H1PC/H1CE/H2X/H3B/H3/ODST cache files.")
         {
@@ -31,37 +39,30 @@ namespace TagTool.Commands.Models
             if (args.Count != 2)
                 return false;
 
-            throw new NotImplementedException();
-            /*
-            var initialStringIDCount = Info.StringIDs.Strings.Count;
+            var cacheFileInfo = new FileInfo(args[0]);
 
-            bool isNew = false;
-            if (args.Count == 3)
-            {
-                if (args[0] != "new")
-                    return false;
-                isNew = true;
-                args.Remove("new");
-            }
+            if (!cacheFileInfo.Exists)
+                throw new FileNotFoundException(cacheFileInfo.FullName);
 
-            if (args.Count != 2)
-                return false;
+            var cacheFile = CacheManager.GetCache(cacheFileInfo.FullName);
 
+            var initialStringIDCount = CacheContext.StringIdCache.Strings.Count;
+            
             //
             // Verify the Blam render_model tag
             //
 
-            var renderModelName = args[0];
+            var renderModelName = args[1];
 
-            CacheFile.IndexItem item = null;
+            CacheBase.IndexItem item = null;
 
             Console.WriteLine("Verifying Blam tag...");
 
-            foreach (var tag in BlamCache.IndexItems)
+            foreach (var indexItem in cacheFile.IndexItems)
             {
-                if (tag.ClassCode == "mode" && tag.Filename == renderModelName)
+                if (indexItem.ClassCode == "mode" && indexItem.Filename == renderModelName)
                 {
-                    item = tag;
+                    item = indexItem;
                     break;
                 }
             }
@@ -75,21 +76,16 @@ namespace TagTool.Commands.Models
             //
             // Verify the ED render_model tag
             //
-
-            Console.WriteLine("Verifying ED tag index...");
-
-            int edRenderModelIndex;
-
-            if (!int.TryParse(args[1], NumberStyles.HexNumber, null, out edRenderModelIndex) ||
-                (edRenderModelIndex >= Info.Cache.Tags.Count))
+            
+            if (Definition.RenderModel == null)
             {
-                Console.WriteLine("Invalid tag index: " + args[1]);
-                return false;
+                Definition.RenderModel = CacheContext.TagCache.AllocateTag(
+                    new TagGroup(new Tag("mode"), Common.Tag.Null, Common.Tag.Null, CacheContext.GetStringId("render_model")));
             }
 
-            var edTag = Info.Cache.Tags[edRenderModelIndex];
+            var renderModelTag = Definition.RenderModel;
 
-            if (edTag.Group.Name != Info.StringIDs.GetStringID("render_model"))
+            if (renderModelTag.Group.Name != CacheContext.GetStringId("render_model"))
             {
                 Console.WriteLine("Specified tag index is not a render_model: " + args[1]);
                 return false;
@@ -101,18 +97,18 @@ namespace TagTool.Commands.Models
 
             Console.WriteLine("Loading ED render_model tag...");
 
-            TagDefinitions.RenderModel renderModel;
+            RenderModel renderModel;
 
-            using (var cacheStream = Info.CacheFile.Open(FileMode.Open, FileAccess.ReadWrite))
+            using (var cacheStream = CacheContext.TagCacheFile.Open(FileMode.Open, FileAccess.ReadWrite))
             {
                 try
                 {
-                    var context = new Serialization.TagSerializationContext(cacheStream, Info.Cache, Info.StringIDs, Info.Cache.Tags[(int)edRenderModelIndex]);
-                    renderModel = Info.Deserializer.Deserialize<TagDefinitions.RenderModel>(context);
+                    var context = new TagSerializationContext(cacheStream, CacheContext, renderModelTag);
+                    renderModel = CacheContext.Deserializer.Deserialize<RenderModel>(context);
                 }
                 catch
                 {
-                    Console.WriteLine("Failed to deserialize selected render_model tag: " + edRenderModelIndex);
+                    Console.WriteLine($"Failed to deserialize selected render_model tag: 0x{renderModelTag.Index:X4}");
                     return true;
                 }
             }
@@ -121,57 +117,34 @@ namespace TagTool.Commands.Models
             // Load the Blam render_model tag raw
             //
             
-            var mode = DefinitionsManager.mode(BlamCache, item);
+            var mode = DefinitionsManager.mode(cacheFile, item);
             mode.LoadRaw();
-
-            //
-            // Duplicate the render_model tag we're injecting over
-            //
-
-            TagInstance newTag;
-
-            if (isNew)
-            {
-                Console.WriteLine("Duplicating selected render_model tag...");
-
-                if (!new DuplicateTagCommand(Info).Execute(new List<string> { edRenderModelIndex.ToString("X8") }))
-                {
-                    Console.WriteLine("Failed to duplicate render_model tag: " + edRenderModelIndex);
-                    return false;
-                }
-
-                newTag = Info.Cache.Tags[Info.Cache.Tags.Count - 1];
-            }
-            else
-            {
-                newTag = edTag;
-            }
-
+            
             //
             // Start porting the model
             //
 
-            var builder = new RenderModelBuilder(DefinitionSet.HaloOnline106708);
+            var builder = new RenderModelBuilder(CacheContext.Version);
             
             foreach (var node in mode.Nodes)
             {
-                var nodeNameId = Info.StringIDs.GetStringID(node.Name);
+                var nodeNameId = CacheContext.GetStringId(node.Name);
 
                 builder.AddNode(
-                    new TagDefinitions.RenderModel.Node
+                    new RenderModel.Node
                     {
-                        Name = nodeNameId.Index == 0 && node.Name != "" ? nodeNameId = Info.StringIDs.Add(node.Name) : nodeNameId,
+                        Name = nodeNameId.Index == 0 && node.Name != "" ? nodeNameId = CacheContext.StringIdCache.Add(node.Name) : nodeNameId,
                         ParentNode = (short)node.ParentIndex,
                         FirstChildNode = (short)node.FirstChildIndex,
                         NextSiblingNode = (short)node.NextSiblingIndex,
                         ImportNode = 0,
-                        DefaultTranslation = new Common.Vector3(node.Position.X, node.Position.Y, node.Position.Z),
-                        DefaultRotation = new Common.Vector4(node.Rotation.X, node.Rotation.Y, node.Rotation.Z, node.Rotation.W),
+                        DefaultTranslation = new RealPoint3d(node.Position.I, node.Position.J, node.Position.K),
+                        DefaultRotation = new RealVector4d(node.Rotation.I, node.Rotation.J, node.Rotation.K, node.Rotation.W),
                         DefaultScale = node.TransformScale,
-                        InverseForward = new Common.Vector3(node.TransformMatrix.m11, node.TransformMatrix.m12, node.TransformMatrix.m13),
-                        InverseLeft = new Common.Vector3(node.TransformMatrix.m21, node.TransformMatrix.m22, node.TransformMatrix.m23),
-                        InverseUp = new Common.Vector3(node.TransformMatrix.m31, node.TransformMatrix.m32, node.TransformMatrix.m33),
-                        InversePosition = new Common.Vector3(node.TransformMatrix.m41, node.TransformMatrix.m42, node.TransformMatrix.m43),
+                        InverseForward = new RealPoint3d(node.TransformMatrix.m11, node.TransformMatrix.m12, node.TransformMatrix.m13),
+                        InverseLeft = new RealPoint3d(node.TransformMatrix.m21, node.TransformMatrix.m22, node.TransformMatrix.m23),
+                        InverseUp = new RealPoint3d(node.TransformMatrix.m31, node.TransformMatrix.m32, node.TransformMatrix.m33),
+                        InversePosition = new RealPoint3d(node.TransformMatrix.m41, node.TransformMatrix.m42, node.TransformMatrix.m43),
                         DistanceFromParent = node.DistanceFromParent
                     });
             }
@@ -184,7 +157,7 @@ namespace TagTool.Commands.Models
                 builder.AddMaterial(
                     new RenderMaterial
                     {
-                        RenderMethod = Info.Cache.Tags[0x101F]
+                        RenderMethod = CacheContext.TagCache.Index[0x101F]
                     });
 
             //
@@ -193,18 +166,18 @@ namespace TagTool.Commands.Models
 
             foreach (var region in mode.Regions)
             {
-                var regionNameId = Info.StringIDs.GetStringID(region.Name);
+                var regionNameId = CacheContext.GetStringId(region.Name);
 
-                builder.BeginRegion(regionNameId.Index == 0 && region.Name != "" ? regionNameId = Info.StringIDs.Add(region.Name) : regionNameId);
+                builder.BeginRegion(regionNameId.Index == 0 && region.Name != "" ? regionNameId = CacheContext.StringIdCache.Add(region.Name) : regionNameId);
 
                 foreach (var permutation in region.Permutations)
                 {
                     if (permutation.PieceCount <= 0 || permutation.PieceIndex == -1)
                         continue;
 
-                    var permutationNameId = Info.StringIDs.GetStringID(permutation.Name);
+                    var permutationNameId = CacheContext.GetStringId(permutation.Name);
 
-                    builder.BeginPermutation(permutationNameId.Index == 0 && permutation.Name != "" ? permutationNameId = Info.StringIDs.Add(permutation.Name) : permutationNameId);
+                    builder.BeginPermutation(permutationNameId.Index == 0 && permutation.Name != "" ? permutationNameId = CacheContext.StringIdCache.Add(permutation.Name) : permutationNameId);
 
                     for (var i = permutation.PieceIndex; i < permutation.PieceIndex + permutation.PieceCount; i++)
                     {
@@ -227,19 +200,19 @@ namespace TagTool.Commands.Models
                         foreach (var vertex in section.Vertices)
                         {
                             vertex.TryGetValue("position", 0, out v);
-                            var position = new Common.Vector4(v.Data.X, v.Data.Y, v.Data.Z, 1);
+                            var position = new RealVector4d(v.Data.I, v.Data.J, v.Data.K, 1);
 
                             vertex.TryGetValue("normal", 0, out v);
-                            var normal = new Common.Vector3(v.Data.I, v.Data.J, v.Data.K);
+                            var normal = new RealPoint3d(v.Data.I, v.Data.J, v.Data.K);
 
                             vertex.TryGetValue("texcoords", 0, out v);
-                            var texcoord = new Common.Vector2(v.Data.X, v.Data.Y);
+                            var texcoord = new RealPoint2d(v.Data.I, v.Data.J);
 
                             vertex.TryGetValue("tangent", 0, out v);
-                            var tangent = new Common.Vector4(v.Data.X, v.Data.Y, v.Data.Z, 1);
+                            var tangent = new RealVector4d(v.Data.I, v.Data.J, v.Data.K, 1);
 
                             vertex.TryGetValue("binormal", 0, out v);
-                            var binormal = new Common.Vector3(v.Data.X, v.Data.Y, v.Data.Z);
+                            var binormal = new RealPoint3d(v.Data.I, v.Data.J, v.Data.K);
 
                             rigidVertices.Add(
                                 new RigidVertex
@@ -257,10 +230,10 @@ namespace TagTool.Commands.Models
 
                                 vertex.TryGetValue("blendindices", 0, out v);
 
-                                blendIndices.Add((byte)v.Data.A);
-                                blendIndices.Add((byte)v.Data.B);
-                                blendIndices.Add((byte)v.Data.C);
-                                blendIndices.Add((byte)v.Data.D);
+                                blendIndices.Add((byte)v.Data.I);
+                                blendIndices.Add((byte)v.Data.J);
+                                blendIndices.Add((byte)v.Data.K);
+                                blendIndices.Add((byte)v.Data.W);
 
                                 skinnedVertices.Add(new SkinnedVertex
                                 {
@@ -280,17 +253,17 @@ namespace TagTool.Commands.Models
 
                                 vertex.TryGetValue("blendindices", 0, out v);
 
-                                blendIndices.Add((byte)v.Data.A);
-                                blendIndices.Add((byte)v.Data.B);
-                                blendIndices.Add((byte)v.Data.C);
-                                blendIndices.Add((byte)v.Data.D);
+                                blendIndices.Add((byte)v.Data.I);
+                                blendIndices.Add((byte)v.Data.J);
+                                blendIndices.Add((byte)v.Data.K);
+                                blendIndices.Add((byte)v.Data.W);
 
                                 vertex.TryGetValue("blendweight", 0, out v);
 
-                                blendWeights.Add(v.Data.A);
-                                blendWeights.Add(v.Data.B);
-                                blendWeights.Add(v.Data.C);
-                                blendWeights.Add(v.Data.D);
+                                blendWeights.Add(v.Data.I);
+                                blendWeights.Add(v.Data.J);
+                                blendWeights.Add(v.Data.K);
+                                blendWeights.Add(v.Data.W);
 
                                 skinnedVertices.Add(new SkinnedVertex
                                 {
@@ -349,40 +322,40 @@ namespace TagTool.Commands.Models
             //
 
             var resourceStream = new MemoryStream();
-            var newRenderModel = builder.Build(Info.Serializer, resourceStream);
+            var newRenderModel = builder.Build(CacheContext.Serializer, resourceStream);
 
-            var renderModelNameStringID = Info.StringIDs.GetStringID(mode.Name);
+            var renderModelNameStringID = CacheContext.GetStringId(mode.Name);
 
             newRenderModel.Name = renderModelNameStringID.Index == -1 ?
-                renderModelNameStringID = Info.StringIDs.Add(mode.Name) :
+                renderModelNameStringID = CacheContext.StringIdCache.Add(mode.Name) :
                 renderModelNameStringID;
 
             //
             // Add the markers to the new render_model
             //
 
-            newRenderModel.MarkerGroups = new List<TagDefinitions.RenderModel.MarkerGroup>();
+            newRenderModel.MarkerGroups = new List<RenderModel.MarkerGroup>();
             
             foreach (var markerGroup in mode.MarkerGroups)
             {
-                var markerGroupNameId = Info.StringIDs.GetStringID(markerGroup.Name);
+                var markerGroupNameId = CacheContext.GetStringId(markerGroup.Name);
 
                 if (markerGroupNameId.Index == -1)
-                    markerGroupNameId = Info.StringIDs.Add(markerGroup.Name);
+                    markerGroupNameId = CacheContext.StringIdCache.Add(markerGroup.Name);
 
                 newRenderModel.MarkerGroups.Add(
-                    new TagDefinitions.RenderModel.MarkerGroup
+                    new RenderModel.MarkerGroup
                     {
                         Name = markerGroupNameId,
                         Markers = markerGroup.Markers.Select(marker =>
-                            new TagDefinitions.RenderModel.MarkerGroup.Marker
+                            new RenderModel.MarkerGroup.Marker
                             {
                                 RegionIndex = (sbyte)marker.RegionIndex,
                                 PermutationIndex = (sbyte)marker.PermutationIndex,
                                 NodeIndex = (sbyte)marker.NodeIndex,
                                 Unknown3 = 0,
-                                Translation = new Common.Vector3(marker.Position.X, marker.Position.Y, marker.Position.Z),
-                                Rotation = new Common.Vector4(marker.Rotation.X, marker.Rotation.Y, marker.Rotation.Z, marker.Rotation.W),
+                                Translation = new RealPoint3d(marker.Position.I, marker.Position.J, marker.Position.K),
+                                Rotation = new RealVector4d(marker.Rotation.I, marker.Rotation.J, marker.Rotation.K, marker.Rotation.W),
                                 Scale = marker.Scale
                             }).ToList()
                     });
@@ -403,18 +376,21 @@ namespace TagTool.Commands.Models
             Console.WriteLine("Writing resource data...");
 
             var resources = new ResourceDataManager();
-            resources.LoadCachesFromDirectory(Info.CacheFile.DirectoryName);
+            resources.LoadCachesFromDirectory(CacheContext.TagCacheFile.DirectoryName);
             resourceStream.Position = 0;
             resources.Add(newRenderModel.Geometry.Resource, ResourceLocation.Resources, resourceStream);
 
-            using (var cacheStream = Info.CacheFile.Open(FileMode.Open, FileAccess.ReadWrite))
+            using (var cacheStream = CacheContext.TagCacheFile.Open(FileMode.Open, FileAccess.ReadWrite))
             {
                 Console.WriteLine("Writing tag data...");
 
-                newRenderModel.Geometry.Resource.Owner = newTag;
+                newRenderModel.Geometry.Resource.Owner = renderModelTag;
 
-                var context = new Serialization.TagSerializationContext(cacheStream, Info.Cache, Info.StringIDs, newTag);
-                Info.Serializer.Serialize(context, newRenderModel);
+                var context = new TagSerializationContext(cacheStream, CacheContext, renderModelTag);
+                CacheContext.Serializer.Serialize(context, newRenderModel);
+
+                context = new TagSerializationContext(cacheStream, CacheContext, Tag);
+                CacheContext.Serializer.Serialize(context, Definition);
             }
 
             resourceStream.Close();
@@ -423,12 +399,12 @@ namespace TagTool.Commands.Models
             // Save new string_ids
             //
 
-            if (Info.StringIDs.Strings.Count != initialStringIDCount)
+            if (CacheContext.StringIdCache.Strings.Count != initialStringIDCount)
             {
                 Console.WriteLine("Saving string_ids...");
 
-                using (var stringIdStream = Info.StringIDsFile.Open(FileMode.Open, FileAccess.ReadWrite))
-                    Info.StringIDs.Save(stringIdStream);
+                using (var stringIdStream = CacheContext.OpenStringIdCacheReadWrite())
+                    CacheContext.StringIdCache.Save(stringIdStream);
             }
 
             //
@@ -438,7 +414,6 @@ namespace TagTool.Commands.Models
             Console.WriteLine("Ported render_model \"" + renderModelName + "\" successfully!");
 
             return true;
-            */
         }
     }
 }
